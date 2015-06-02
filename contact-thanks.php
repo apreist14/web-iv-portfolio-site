8acc8a772242e0df1e601fb2b02da0a7b7443545
apreist14 <b6:84:e1:1b:ca:ef:c2:d8:bc:8c:65:4e:aa:28:84:1e>
2015-06-02 17:48:01 -0500

    navigation and button

--- contact-thanks.php
+++ contact-thanks.php
@@ -23,12 +23,7 @@
 	
 	</div>
 	
-	<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
-	</script>
-	<script type="text/javascript">
-	_uacct = "UA-68528-29";
-	urchinTracker();
-	</script>
+
 
 </body>
 
