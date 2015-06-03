588f559fcc6a5d521d61e971e78fdfd8cae3142d
apreist14 <b6:84:e1:1b:ca:ef:c2:d8:bc:8c:65:4e:aa:28:84:1e>
2015-06-03 12:19:06 -0500

    styling thank you form

--- contact-thanks.php
+++ contact-thanks.php
@@ -1,27 +1,74 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Strict//EN">
 
 <head>
+<script src="scripts/jquery-2.1.4.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="styles/foundation.css">
+    <link rel="stylesheet" type="text/css" href="styles/custom.css">
+    <link href='http://fonts.googleapis.com/css?family=Raleway:400,500,600,700,300' rel='stylesheet' type='text/css'>
+    <script src="scripts/vendor/modernizr.js"></script>
+    <script src="scripts/custom.js"></script>
 	<title>A Nice & Simple Contact Form</title>
-	
 	<link rel="stylesheet" type="text/css" href="style.css" />
 </head>
 
 <body>
 
-	<div id="page-wrap">
+<header>
+      <a id="home"></a>
+      <div class="row">
+        <div class="contatiner">
+          <nav id="navigation">
+            <div class="medium-12 medium-offset-4">
+              <div class="nav-trigger">Menu<span></span></div>
+              <ul class="menu">
+                <li><a href="#home">home</a> </li>
+                <li><a href="#projects">projects</a></li>
+                <li class="hide-for-medium hide-for-small"> <a href="index.html"><img src="images/ap-logo.svg" alt="ashley preister"></a></li>
+                <li><a href="#about">about</a></li>
+                <li><a href="#contact">contact</a></li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </header>
 
-		<img src="images/title.gif" alt="A Nice & Simple Contact Form" />
-		<p>By <a href="http://css-tricks.com">CSS-Tricks</a></p>
-			
-			
-		<br /><br />
+	<main>
+
+		<div class="thank-you"	
+		<div class="row">
+        <div class="small-12 small-text-center">
+        <h1>Thank You!</h1>
+        <p>Your message has been sent!</p>
+        
+        </div>
+        </div>
 		
-			
-		<h1>Your message has been sent!</h1><br />
-		
+        <div class="row">
+        <div class="info small-12 small-text-center">
 		<p><a href="index.html">Back to Contact Form</a></p>
+        </div>
+        </div>
 	
-	</div>
+	</main>
+    
+        <footer>
+      <div class="row footer">
+        <div class="large-4 medium-4 columns">
+          <p>&copy; Copyright Ashley Preister</p>
+        </div>
+        <div class="info large-4 medium-4 columns">
+          <a href="mailto:hello@ashleypreister.com">
+            <p>hello@ashleypreister.com</p>
+          </a>
+        </div>
+        <div class="info large-4 medium-4 columns">
+          <a href="tel:1(402)-920-1433">
+            <p>402.920.1433</p>
+          </a>
+        </div>
+      </div>
+    </footer>
 	
 
 
