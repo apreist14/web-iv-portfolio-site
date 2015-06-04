a91b5f7d9bcd7f277097b662583330fb6064547d
apreist14 <b6:84:e1:1b:ca:ef:c2:d8:bc:8c:65:4e:aa:28:84:1e>
2015-06-04 14:47:32 -0500

    styling the form

--- contact-engine.php
+++ contact-engine.php
@@ -18,13 +18,13 @@ if (!$validationOK) {
 // prepare email body text
 $Body = "";
 $Body .= "Name: ";
-$Body .= $Name;
+$Body .= $name;
 $Body .= "\n";
 $Body .= "Email: ";
-$Body .= $Email;
+$Body .= $email;
 $Body .= "\n";
 $Body .= "Message: ";
-$Body .= $Message;
+$Body .= $message;
 $Body .= "\n";
 
 // send email 
